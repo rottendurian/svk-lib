7eb4898c56f22b708fd788eec7e740ff3716092c
Ryan <rmw071102@gmail.com>
2023-04-23 14:19:48 -0400

    removed virtual threadpool interface for performance reasons, the same effect can be acheived with static variables

--- svklib/svk_threadpool.cpp
+++ svklib/svk_threadpool.cpp
@@ -1,59 +1,38 @@
 #ifndef SVKLIB_THREADPOOL_CPP
 #define SVKLIB_THREADPOOL_CPP
 
-#include "svklib.hpp"
+#include "svk_threadpool.hpp"
 
 #define STP_POOL_IMPLEMENTATION
 #include "stp.hpp"
 
 namespace svklib {
 
-class threadpoolImpl : public threadpool {
-public:
-    threadpoolImpl(threadpoolImpl const&) = delete;
-    void operator=(threadpoolImpl const&) = delete;
+static stp::pool m_pool;
+static std::mutex m_mutex;
 
-    static threadpoolImpl* get_instance();
-    void add_task(std::function<void()> task);
-    void add_task(std::function<void()> task,std::atomic_bool* isComplete);
-    void wait_for_tasks();
-    void empty();
-    void has_tasks();
+threadpool::threadpool() {} 
 
-private:
-    friend class std::unique_ptr<threadpoolImpl>;
-    threadpoolImpl();
-    // threadpoolImpl(threadpoolImpl const&); // Don't Implement
-    // void operator=(threadpoolImpl const&); // Don't implement
-    static std::mutex m_mutex;
-    stp::pool m_pool;
-};
-
-std::mutex threadpoolImpl::m_mutex;
-
-threadpoolImpl::threadpoolImpl() 
-    :m_pool(stp::pool()) {}
-
-threadpoolImpl* threadpoolImpl::get_instance() {
-    static std::unique_ptr<threadpoolImpl> instance;
+threadpool* threadpool::get_instance() {
+    static std::unique_ptr<threadpool> instance;
 
     if (instance == nullptr) {
 
         std::lock_guard<std::mutex> lock(m_mutex);
 
         if (instance == nullptr) {
-            instance.reset(new threadpoolImpl());
+            instance.reset(new threadpool());
         }
     }
 
     return instance.get();
 }
 
-void threadpoolImpl::add_task(std::function<void()> task) {
+void threadpool::add_task(std::function<void()> task) {
     m_pool.add_task(task);
 }
 
-void threadpoolImpl::add_task(std::function<void()> task,std::atomic_bool* isComplete) {
+void threadpool::add_task(std::function<void()> task,std::atomic_bool* isComplete) {
     isComplete->store(false);
     m_pool.add_task([task,isComplete](){
         task();
@@ -61,22 +40,17 @@ void threadpoolImpl::add_task(std::function<void()> task,std::atomic_bool* isCom
     });
 }
 
-void threadpoolImpl::wait_for_tasks() {
+void threadpool::wait_for_tasks() {
     m_pool.wait_for_tasks();
 }
 
-void threadpoolImpl::empty() {
+void threadpool::empty() {
     m_pool.empty();
 }
 
-void threadpoolImpl::has_tasks() {
+void threadpool::has_tasks() {
     m_pool.has_tasks();
 }
-
-threadpool* threadpool::get_instance() {
-    threadpool* pool = threadpoolImpl::get_instance();
-    return pool;
-}    
 
 } // namespace svklib
 
--- svklib/svk_threadpool.hpp
+++ svklib/svk_threadpool.hpp
@@ -1,26 +1,23 @@
 #ifndef SVKLIB_THREADPOOL_HPP
 #define SVKLIB_THREADPOOL_HPP
 
-// #include "pch.hpp"
-
 namespace svklib {
 
 class threadpool {
-private:
-    friend class threadpoolImpl;
 public:
     threadpool(threadpool const&) = delete;
     void operator=(threadpool const&) = delete;
 
     static threadpool* get_instance();
-    virtual void add_task(std::function<void()> task) = 0;
-    virtual void add_task(std::function<void()> task,std::atomic_bool* isComplete) = 0;
-    virtual void wait_for_tasks() = 0;
-    virtual void empty() = 0;
-    virtual void has_tasks() = 0;
+    void add_task(std::function<void()> task);
+    void add_task(std::function<void()> task,std::atomic_bool* isComplete);
+    void wait_for_tasks();
+    void empty();
+    void has_tasks();
 
 private:
-    threadpool() {}
+    threadpool();
+
     // threadpool(threadpool const&);     // Don't Implement
     // void operator=(threadpool const&); // Don't implement
 };
